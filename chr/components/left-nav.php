8795b5b74a95765936c44395203156e49ef2f7f2
Christian Alia <121612235+Christian24GH@users.noreply.github.com>
2024-10-26 08:12:48 +0800

    Delete chr directory

--- chr/components/left-nav.php
+++
@@ -1,6 +0,0 @@
-<div class="offcanvas offcanvas-start" tabindex="-1" id="sidebar-offcanvas">
-    <div class="offcanvas-header">
-        <h5 class="offcanvas-title" id="offcanvasExampleLabel">Offcanvas</h5>
-        <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-    </div>
-</div>
--- chr/conn.php
+++
@@ -1,7 +0,0 @@
-<?php
-    try{
-        $conn = new mysqli("localhost", "root", "");
-    }catch(mysqli_sql_exception){
-        die("<div class='conn_err'> Connection Failed </div>");
-    }
-?>
