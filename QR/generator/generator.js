741636f4eb12f9e56d8f824eebf4d4d9ace223a8
Christian24GH <loquezchristian@gmail.com>
2024-10-29 14:31:12 +0800

    asd

--- QR/generator/generator.js
+++
@@ -1,6 +0,0 @@
-let qrBox = document.getElementById("qrBox");
-let text = document.getElementById("text");
-
-document.getElementById("generateBtn").addEventListener("click", ()=>{
-    qrBox.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${text.value}`;
-});
